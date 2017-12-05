bee7a71a78e983ec7fc63cffc8814fef2b30bd4c
Lukas Radimersky <radimersky@live.com>
2017-12-05 11:36:35 +0100

    Doplnění databáze o timestamps

--- .database/sp.sql
+++ .database/sp.sql
@@ -3,11 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1:3306
--- Vytvořeno: Ned 03. pro 2017, 20:18
+-- Vytvořeno: Úte 05. pro 2017, 10:34
 -- Verze serveru: 5.7.19
 -- Verze PHP: 5.6.31
 
-SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -40,11 +39,20 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `a_abstract` varchar(500) COLLATE utf8_czech_ci NOT NULL,
   `a_filename` varchar(25) COLLATE utf8_czech_ci NOT NULL,
   `a_state` int(11) NOT NULL,
-  `a_exist` tinyint(1) NOT NULL,
+  `a_exist` tinyint(1) NOT NULL DEFAULT '1',
   `a_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_article`),
-  KEY `set_state` (`a_state`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='Tabulka prispevku v konferenci';
+  KEY `set_state` (`a_state`),
+  KEY `id_user` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='Tabulka prispevku v konferenci';
+
+--
+-- Vypisuji data pro tabulku `articles`
+--
+
+INSERT INTO `articles` (`id_article`, `id_user`, `a_name`, `a_abstract`, `a_filename`, `a_state`, `a_exist`, `a_date`) VALUES
+(1, 2, 'Pojednani o jahodove zmrzline', 'Jahodova zmrzlina', 'zmrzlina.pdf', 1, 1, '2017-12-03 22:00:45'),
+(2, 2, 'Recept na citronovou limonádu', 'Prastarý cectpt, jak si doma utvořit tuto vzácnou pochutinu', 'citronada.pdf', 1, 1, '2017-12-03 22:00:45');
 
 -- --------------------------------------------------------
 
@@ -122,18 +130,22 @@ CREATE TABLE IF NOT EXISTS `users` (
   `passwd` varchar(20) COLLATE utf8_czech_ci NOT NULL,
   `email` varchar(25) COLLATE utf8_czech_ci NOT NULL,
   `id_right` int(11) NOT NULL DEFAULT '1' COMMENT 'Typ puzivatelskych prav',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `exist` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id_user`),
   KEY `id_right` (`id_right`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='Tabulka vsech uzivatelu';
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='Tabulka vsech uzivatelu';
 
 --
 -- Vypisuji data pro tabulku `users`
 --
 
-INSERT INTO `users` (`id_user`, `name`, `nick`, `passwd`, `email`, `id_right`, `exist`) VALUES
-(1, 'Administrátor', 'admin', 'admin', 'admin@admin.com', 4, 1),
-(2, 'Lukáš Radimerský', 'radluk', 'jahoda', 'radluk@students.zcu.cz', 1, 1);
+INSERT INTO `users` (`id_user`, `name`, `nick`, `passwd`, `email`, `id_right`, `created`, `exist`) VALUES
+(1, 'Administrátor', 'admin', 'admin', 'admin@admin.com', 4, '2017-12-04 18:56:28', 1),
+(2, 'Lukáš Radimerský', 'radluk', 'jahoda', 'radluk@students.zcu.cz', 2, '2017-12-04 18:56:28', 1),
+(3, 'Tomáš Recenzeznáš', 'rec01', 'rec01', 'rec01@radimersky.cz', 3, '2017-12-04 18:59:06', 1),
+(4, 'Kamil Všechnočet', 'rec02', 'rec02', 'rec02@radimersky.cz', 3, '2017-12-04 18:59:06', 1),
+(5, 'Luděk Pochvička', 'rec03', 'rec03', 'rec03@radimersky.cz', 3, '2017-12-04 18:59:06', 1);
 
 --
 -- Omezení pro exportované tabulky
@@ -143,6 +155,7 @@ INSERT INTO `users` (`id_user`, `name`, `nick`, `passwd`, `email`, `id_right`, `
 -- Omezení pro tabulku `articles`
 --
 ALTER TABLE `articles`
+  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `set_state` FOREIGN KEY (`a_state`) REFERENCES `states` (`id_state`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
@@ -157,7 +170,6 @@ ALTER TABLE `evaluation`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_right`) REFERENCES `rights` (`id_right`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
